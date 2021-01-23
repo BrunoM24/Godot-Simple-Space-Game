ebfbb31862f133d2515ec9053ef9a7825be84f4d
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-23 20:39:22 +0000

    MouseTarget - Improved the logic to reset the animation

--- src/UI/MouseTarget.gd
+++ src/UI/MouseTarget.gd
@@ -11,4 +11,8 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
 		global_position = get_global_mouse_position()
+		
+		if animation_player.is_playing():
+			animation_player.stop()
+		
 		animation_player.play("fade_out")
