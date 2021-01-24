b29e027e491713858728c901c43b1f5b417afb55
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-24 21:51:27 +0000

    StateMachine - Implemented the StateMachine Logic

--- src/state_machine/StateMachine.gd
+++ src/state_machine/StateMachine.gd
@@ -5,3 +5,38 @@ class_name StateMachine, "res://assets/engine/state_machine.svg"
 Generic State Machine. Intializes states and delegates engine callbacks
 (_physics_process, _unhandle_input) to the active state.
 """
+
+
+export var initial_state := NodePath()
+
+onready var state: State = get_node(initial_state) setget set_state
+onready var _state_name: String = state.name
+
+
+func _ready() -> void:
+	yield(owner, "ready")
+	state.enter()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	state.unhandle_input(event)
+
+
+func _physics_process(delta: float) -> void:
+	state.physics_process(delta)
+
+
+func transition_to(state_path: String, msg: Dictionary = {}) -> void:
+	if not has_node(state_path):
+		return
+	
+	state.exit()
+	
+	self.state = get_node(state_path)
+	
+	state.enter(msg)
+
+
+func set_state(value: State) -> void:
+	state = value
+	_state_name = state.name
