c09fc2f0a114644105376cb33f687bd45033d0f9
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-24 22:00:24 +0000

    StateMachine - State - Implemented the State Basic Interface

--- src/state_machine/State.gd
+++ src/state_machine/State.gd
@@ -4,3 +4,26 @@ class_name State, "res://assets/engine/state.svg"
 """
 State Interface to use in Hierarchical State Machines.
 """
+
+
+func unhandled_input(event: InputEvent) -> void:
+	pass
+
+
+func physics_process(delta: float) -> void:
+	pass
+
+
+func enter(msg: Dictionary = {}) -> void:
+	pass
+
+
+func exit() -> void:
+	pass
+
+
+func _get_state_machine(node: Node) -> Node:
+	if node != null && not node.is_class("StateMachine"):
+		return _get_state_machine(node.get_parent())
+	
+	return node
