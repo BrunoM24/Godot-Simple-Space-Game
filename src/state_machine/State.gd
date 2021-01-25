e1bad92bb980e7ec06960a6ae149b07650ed1ff6
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-25 21:02:11 +0000

    State - Fixed the logic to get the StateMachine checking for the group state_machine

--- src/state_machine/State.gd
+++ src/state_machine/State.gd
@@ -5,6 +5,8 @@ class_name State, "res://assets/engine/state.svg"
 State Interface to use in Hierarchical State Machines.
 """
 
+onready var _state_machine := _get_state_machine(self)
+
 
 func unhandled_input(event: InputEvent) -> void:
 	pass
@@ -23,7 +25,7 @@ func exit() -> void:
 
 
 func _get_state_machine(node: Node) -> Node:
-	if node != null && not node.is_class("StateMachine"):
+	if node != null and not node.is_in_group("state_machine"):
 		return _get_state_machine(node.get_parent())
 	
 	return node
