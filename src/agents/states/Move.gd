e993a629fb29094baee264a123294cb23ec0c700
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-25 21:15:42 +0000

    Agents - States - Move - Added the logic to transition to the Idle state when target_position have been reached && Implemented the enter function to set the target_position

--- src/agents/states/Move.gd
+++ src/agents/states/Move.gd
@@ -14,16 +14,14 @@ var _velocity := Vector2.ZERO
 
 func unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
-		_target_position = owner.get_global_mouse_position()
-		set_physics_process(true)
+		_target_position = event.position
 
 
 func physics_process(delta: float) -> void:
 	var distance_to_target : float = owner.global_position.distance_to(_target_position)
 	
 	if distance_to_target < TARGET_TRESHOULD:
-		set_physics_process(false)
-		return
+		_state_machine.transition_to("Idle")
 	
 	var desired_velocity : Vector2 = max_speed * (_target_position - owner.global_position).normalized()
 	
@@ -37,3 +35,8 @@ func physics_process(delta: float) -> void:
 	_velocity = owner.move_and_slide(_velocity)
 	
 	owner.rotation = _velocity.angle()
+
+
+func enter(msg: Dictionary = {}) -> void:
+	if msg.has("target_position"):
+		_target_position = msg.target_position
