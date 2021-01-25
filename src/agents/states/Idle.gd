38d3614a2dbc2841ec72598ff2770e87b8d615d0
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-25 19:50:39 +0000

    Agents - States - Idle State - Implemented the unhundled_input and physics_process to call the parent methods

--- src/agents/states/Idle.gd
+++ src/agents/states/Idle.gd
@@ -1,2 +1,9 @@
 extends State
 
+
+func unhandled_input(event: InputEvent) -> void:
+	get_parent().unhandled_input(event)
+
+
+func physics_process(delta: float) -> void:
+	get_parent().physics_process(delta)
