6a7e1ceba7e9677e85b9e8b346607b8089c010cd
BrunoM24 <brunocostamatos.24@gmail.com>
2021-01-30 23:25:04 +0000

    Agents - States - Follow - Implement the Basic Follow State Logic to follow the Mouse Position

--- src/agents/states/Follow.gd
+++ src/agents/states/Follow.gd
@@ -1,2 +1,37 @@
 extends State
 
+
+const TARGET_TRESHOULD := 4.0
+
+export var max_speed := 500.0
+export var mass := 2.0
+export var slow_radius := 200.0
+
+var _target_position := Vector2.ZERO
+
+var _velocity := Vector2.ZERO
+
+export var avoidance_radius := 400.0
+
+
+func physics_process(delta: float) -> void:
+	_target_position = get_viewport().get_mouse_position()
+	
+	var distance_to_target : float = owner.global_position.distance_to(_target_position)
+	
+	if distance_to_target < TARGET_TRESHOULD:
+		return
+	
+	_velocity = _calculate_velocity(_velocity, owner.global_position, _target_position, max_speed, mass)
+	
+	_velocity = owner.move_and_slide(_velocity)
+	
+	owner.rotation = _velocity.angle()
+
+
+func _calculate_velocity(velocity: Vector2, position: Vector2, target_position: Vector2, speed: float, mass: float) -> Vector2:
+	var desiered_velocity := (target_position - position).normalized() * speed
+	
+	var steering := (desiered_velocity - velocity) / mass
+	
+	return velocity + steering
